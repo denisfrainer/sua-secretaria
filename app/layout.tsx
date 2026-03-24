c9f44055dd1492a2cf99a61eaa36e8a1e89f5b63
denisfrainer <denisfrainer93@gmail.com>
2026-03-24 10:44:39 -0300

    og fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
         type: 'website',
         images: [
             {
-                url: '/opengraph-image.jpg',
+                url: 'https://wolfagent.netlify.app/og-image.jpg',
                 width: 1200,
                 height: 630,
                 alt: 'Wolf Agency',
