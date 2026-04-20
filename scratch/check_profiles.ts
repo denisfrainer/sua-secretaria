7a061b2fd1c2b237ced200aac8e69a937413d579
denisfrainer <denisfrainer93@gmail.com>
2026-04-20 10:21:27 -0300

    trying to fix booking route

--- scratch/check_profiles.ts
+++ scratch/check_profiles.ts
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from './lib/supabase/admin';
+import { supabaseAdmin } from '../lib/supabase/admin';
 
 async function checkProfiles() {
   const { data, error } = await supabaseAdmin
